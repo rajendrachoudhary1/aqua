1be9ca8ca29c949896e1f062ba9eada634c284ad
rajendrachoudhary1 <itsmerajuchoudhary@gmail.com>
2025-03-06 10:40:23 +0530

    update

--- src/component/homesection/basinslider/Basinslider.jsx
+++ src/component/homesection/basinslider/Basinslider.jsx
@@ -1,7 +1,7 @@
 
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import hero from '../../../assets/basin1.jpg'
+import hero from '../../../assets/basin_img.jpg'
 import hero1 from '../../../assets/basin2.jpg'
  
 import Basin from "../basin/Basin";
