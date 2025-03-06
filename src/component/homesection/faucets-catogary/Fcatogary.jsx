49d094abf4aae3e67550b53048e3f360ef504fc2
rajendrachoudhary1 <itsmerajuchoudhary@gmail.com>
2025-03-06 10:20:01 +0530

    update

--- src/component/homesection/faucets-catogary/Fcatogary.jsx
+++ src/component/homesection/faucets-catogary/Fcatogary.jsx
@@ -1,14 +1,74 @@
-import React from 'react'
-import './Fcatogary.css'
+import React from "react";
+import "./Fcatogary.css";
+import icon1 from "../../../assets/faucets_icon/faucet.png";
+import icon2 from "../../../assets/faucets_icon/faucet1.png";
+import icon3 from "../../../assets/faucets_icon/faucet2.png";
+import icon4 from "../../../assets/faucets_icon/faucet3.png";
+import icon5 from "../../../assets/faucets_icon/faucet4.png";
+import icon6 from "../../../assets/faucets_icon/tap.png";
 const Fcatogary = () => {
   return (
-    <div className='f-info'>
-      <p className='f-info-p'>Faucets-series</p>
-      <p><span>FLT |</span> <span>SCORPIUS |</span> <span>CANIS |</span>
-      <span>AURIGA |</span> <span>DUET|</span> <span>IDEAL |</span> <span>ICON |</span>
-      <span>NANO |</span> <span>SLIKA |</span> <span>JIVA |</span> <span>TURBO |</span></p>
-    </div>
-  )
-}
+    <>
+      <div className="icon-main-container">
+        <h3>FAUCET-SERIES</h3>
+        <div className="icon-container">
+          <div className="icon-container-div">
+            <div className="f-info">
+              <div className="faucet_icon">
+                {" "}
+                <img src={icon1} alt="" />
+              </div>
+              <div className="underline1"></div>
+              <p>FLT</p>
+            </div>
+            <div className="f-info">
+              <div className="faucet_icon">
+                {" "}
+                <img src={icon2} alt="" />
+              </div>
+              <div className="underline1"></div>
+              <p>VIA</p>
+            </div>
+            <div className="f-info">
+              <div className="faucet_icon">
+                {" "}
+                <img src={icon3} alt="" />
+              </div>
+              <div className="underline1"></div>
+              <p>IDEAL</p>
+            </div>
+          </div>
 
-export default Fcatogary
+          <div div className="icon-container-div">
+            <div className="f-info">
+              <div className="faucet_icon">
+                {" "}
+                <img src={icon4} alt="" />
+              </div>
+              <div className="underline1"></div>
+              <p>DUCK</p>
+            </div>
+            <div className="f-info">
+              <div className="faucet_icon">
+                {" "}
+                <img src={icon5} alt="" />
+              </div>
+              <div className="underline1"></div>
+              <p>SCORPIO</p>
+            </div>
+            <div className="f-info">
+              <div className="faucet_icon">
+                {" "}
+                <img src={icon6} alt="" />
+              </div>
+              <div className="underline1"></div>
+              <p>SCORPIO</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Fcatogary;
