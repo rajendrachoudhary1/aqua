7c2623d7aa3449c988c7d470b45c1073fc1a2d74
rajendrachoudhary1 <itsmerajuchoudhary@gmail.com>
2025-03-08 09:34:59 +0530

    update

--- src/component/productsection/fuacetsproduct/Fproduct.jsx
+++ src/component/productsection/fuacetsproduct/Fproduct.jsx
@@ -1,14 +1,13 @@
-import React from "react";
 import img from "../../../assets/product/backgrd.png";
 import "./Fproduct.css";
 const Fproduct = () => {
   return (
-    <div className="fproduct-container">
-      <div className="fproduct">
+    <div className="fproductContainer">
+      <div className="fproduct_img">
         <img src={img} alt="" />
       </div>
-      <div className="fproduct-title">
-        <h1>FAUCETS COLLECTION</h1>
+      <div className="fproductTitle">
+        {/* <h1>FAUCETS COLLECTION</h1> */}
         <div className="fproduct-sub">
           <a href="">HOME</a>
           <span> | </span>
